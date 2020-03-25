e27838c7283d4cb6566bbfc63132d2f91f87e0ea
develoz <zonadeveloz@gmail.com>
2020-03-25 12:50:16 -0600

    menu gris y componente prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { InfoComponent } from './conponents/info/info.component';
 import { HomeComponent } from './conponents/home/home.component';
 import { ContactComponent } from './conponents/contact/contact.component';
+import { PruebaComponent } from './conponents/prueba/prueba.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'info/:id', component: InfoComponent},
-  {path:'contact', component: ContactComponent}
+  {path:'contact', component: ContactComponent},
+  {path:'prueba', component: PruebaComponent}
+
 ];
 
 @NgModule({
